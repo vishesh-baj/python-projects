349ffa82a9482ab355a02d41bb043832cdfe7373
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-01-18 11:21:00 +0530

    added final coffee machine code

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -36,6 +36,12 @@ def process_coins():
     total += int(input("How many pennies?: ")) * .01
     return total
 
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your Drink {drink_name}")
+
 
 while is_on:
     choice = input("What would you like? espresso/latte/cappuccino: ")
@@ -49,7 +55,10 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink['ingredients']):
             payment = process_coins()
-            is_transaction_successful(payment, drink['cost'])
+            if is_transaction_successful(payment, drink['cost']):
+                make_coffee(choice, drink['ingredients'])
+
+
 
 
 
